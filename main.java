81f7324a1e8b08e7d04a2bc7958bc0570c77454b
shiro_pc <przemek.fied2@gmail.com>
2024-04-06 12:05:41 +0200

    zad2

--- main.java
+++ main.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class main {
-  public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Podaj liczbę uczniów: ");
@@ -10,21 +11,33 @@ public class main {
         System.out.print("Podaj liczbę przedmiotów: ");
         int liczbaPrzedmiotow = scanner.nextInt();
 
-        double[][] oceny = new double[liczbaUczniow][liczbaPrzedmiotow];
+        ArrayList<double[]> listaOcen = dodajOceny(scanner, liczbaUczniow, liczbaPrzedmiotow);
+        wyswietlOcenyISrednie(listaOcen, liczbaPrzedmiotow);
+    }
 
+    public static ArrayList<double[]> dodajOceny(Scanner scanner, int liczbaUczniow, int liczbaPrzedmiotow) {
+        ArrayList<double[]> listaOcen = new ArrayList<>();
         for (int i = 0; i < liczbaUczniow; i++) {
+            double[] ocenyUcznia = new double[liczbaPrzedmiotow];
             for (int j = 0; j < liczbaPrzedmiotow; j++) {
                 System.out.print("Podaj ocenę ucznia " + (i + 1) + " z przedmiotu " + (j + 1) + ": ");
-                oceny[i][j] = scanner.nextDouble();
+                ocenyUcznia[j] = scanner.nextDouble();   
             }
+            listaOcen.add(ocenyUcznia);
         }
+        return listaOcen;
+    }
 
-        for (int i = 0; i < liczbaUczniow; i++) {
+    public static void wyswietlOcenyISrednie(ArrayList<double[]> listaOcen, int liczbaPrzedmiotow) {
+        for (int i = 0; i < listaOcen.size(); i++) {
+            double[] ocenyUcznia = listaOcen.get(i); 
             double sumaOcen = 0;
             for (int j = 0; j < liczbaPrzedmiotow; j++) {
-                sumaOcen += oceny[i][j];
+                sumaOcen += ocenyUcznia[j];
             }
             double sredniaOcen = sumaOcen / liczbaPrzedmiotow;
+            System.out.println("Oceny ucznia " + (i + 1) + ": " + java.util.Arrays.toString(ocenyUcznia));
             System.out.println("Średnia ocen ucznia " + (i + 1) + ": " + sredniaOcen);
+        }
     }
-  }}
+}
